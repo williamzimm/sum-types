f11f80412762be267535e0a1076a77b9ab97f8ec
BettermentBowser <matthewbowser@betterment.com>
2023-10-10 16:42:48 -0400

    init

---
+++ src/test/java/com/langfeatures/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.langfeatures.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
